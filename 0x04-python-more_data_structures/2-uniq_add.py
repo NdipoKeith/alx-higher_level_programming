cb2aa572a72cf28e66b703bbfce7bd3f4ee6024d
NdipoKeith <ndipokeith@gmail.com>
2023-08-16 06:41:06 -0700

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_integers = set()
+    for element in my_list:
+        if isinstance(element, int):
+            unique_integers.add(element)
+    return sum(unique_integers)
