d3d384618e586a2b1994cafd7527857f51b9e83a
NdipoKeith <ndipokeith@gmail.com>
2023-11-30 08:47:27 -0800

    0x10-python-network_0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o temp_response.txt -w "%{http_code}" "$1" > /dev/null && [[ $(cat temp_response.txt) -eq 200 ]] && curl -s "$1"
+# Get response body for URL 200 status code.
+curl -sL "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-        """
-        Find a peak element in a list of unsorted integers.
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
 
-        Args:
-            - list_of_integers: A list of integers
+    if size == 0:
+        return None
 
-        Returns:
-            - A peak element in the list
-            - None if the list is empty
-        """
-
-        if not list_of_integers:
-            return None
-
-        lo, hi = 0, len(list_of_integers) - 1
-
-        while lo < hi:
-            mid = (lo + hi) // 2
-
-            if list_of_integers[mid] > list_of_integers[mid + 1]:
-                hi = mid
-            else:
-                lo = mid + 1
-
-        return list_of_integers[lo]
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
